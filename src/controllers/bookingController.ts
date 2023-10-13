e32a91fa80c65463a2ec1500d3144ec488df59b7
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-14 03:52:40 +0700

    add include and add appointmentDateId

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -5,20 +5,31 @@ const prisma = new PrismaClient()
 
 export const getBooking = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const booking = await prisma.booking.findMany()
+        const booking = await prisma.booking.findMany({
+            include: {
+                car: true,
+                customer: true,
+                serviceType: true,
+            }
+        })
         res.send(booking)
-    } catch (err:any) {
+    } catch (err: any) {
         res.status(err.status || 500).json({ message: err.message });
     }
 }
 
 export const getBookingById = async (req: Request, res: Response, next: NextFunction) => {
-    const { id }  = req.params
+    const { id } = req.params
     try {
         const booking = await prisma.booking.findUnique({
             where: {
                 id: Number(id),
-              },
+            },
+            include: {
+                customer: true,
+                car: true,
+                serviceType: true,
+            }
         });
 
         if (!booking) {
@@ -27,101 +38,100 @@ export const getBookingById = async (req: Request, res: Response, next: NextFunc
 
         res.send(booking)
 
-    } catch (err:any) {
+    } catch (err: any) {
         res.status(err.status || 500).json({ message: err.message });
     }
 }
 
 export const createBooking = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { custId, serviceTypeId, appointmentDateTime, description, licensePlate, carBrand } = req.body
-           
+        const { custId, serviceTypeId, appointmentDateId, appointmentDateTime, description, licensePlate, carBrand } = req.body
+
         //ข้อมูลกรอกไม่ครบ
         if (!custId || !serviceTypeId || !appointmentDateTime || !licensePlate || !carBrand) {
             res.status(400).json({ error: " Please complete the information." });
             return;
         }
-    
+
         //เช็คป้ายทะเบียนว่ามีใน Car ไหม
-    const checkLicensePlate = await prisma.car.findUnique({
-        where: {
-            licensePlate: licensePlate,
-        },
-    });
+        const checkLicensePlate = await prisma.car.findUnique({
+            where: {
+                licensePlate: licensePlate,
+            },
+        });
 
 
-    //เพิ่มข้อมูลเข้า Car, Booking, schedules, Process, Payment
-    const transaction = await prisma.$transaction (async () => {
+        //เพิ่มข้อมูลเข้า Car, Booking, schedules, Process, Payment
+        const transaction = await prisma.$transaction(async () => {
 
-        //สร้าง Car ใหม่
-        let carId: number;
+            //สร้าง Car ใหม่
+            let carId: number;
 
-        if (!checkLicensePlate){
-            const newCar = await prisma.car.create({
+            if (!checkLicensePlate) {
+                const newCar = await prisma.car.create({
+                    data: {
+                        licensePlate: licensePlate,
+                        custId: custId,
+                        carBrand: carBrand
+                    }
+                });
+                carId = newCar.id;
+            } else {
+                carId = checkLicensePlate.id
+            }
+
+            //สร้าง Booking ใหม่
+            const newBooking = await prisma.booking.create({
                 data: {
-                    licensePlate: licensePlate,
                     custId: custId,
-                    carBrand: carBrand
+                    carId: carId,
+                    serviceTypeId: serviceTypeId,
+                    appointmentDateId: appointmentDateId,
+                    appointmentDateTime: appointmentDateTime,
+                    description: description
+                }
+            })
+
+            //สร้าง schedules และหาว่า empId ไหนงานนน้อยสุด
+
+            const minJobEmp = await prisma.employee.findMany({
+                orderBy: {
+                    schedule: {
+                        _count: 'asc' // รายการ schedules น้อยที่สุด
+                    }
                 }
             });
-            carId = newCar.id;
-        } else {
-            carId = checkLicensePlate.id
-        }
 
-        //สร้าง Booking ใหม่
-        const newBooking = await prisma.booking.create({
-            data: {
-                custId: custId,
-                carId: carId,
-                serviceTypeId: serviceTypeId,
-                appointmentDateTime: appointmentDateTime,
-                description: description
-            }
-        })
-
-        //สร้าง schedules และหาว่า empId ไหนงานนน้อยสุด
-
-        const minJobEmp = await prisma.employee.findMany({
-            orderBy: {
-                schedule: {
-                  _count: 'asc' // รายการ schedules น้อยที่สุด
+            const newSchedule = await prisma.schedules.create({
+                data: {
+                    empId: Number(minJobEmp[0].id),
+                    bookingId: newBooking.id,
                 }
-            }
-          });
+            })
 
-        const newSchedule = await prisma.schedules.create ({
-            data: {
-                empId: Number(minJobEmp[0].id),
-                bookingId: newBooking.id,
-            }
+            //สร้าง Process
+            const newProcess = await prisma.process.create({
+                data: {
+                    bookingId: newBooking.id,
+                    subServiceId: 1,
+                    timestamp: new Date(),
+                }
+            })
+
+            //สร้าง Payment
+            const newPayment = await prisma.payment.create({
+                data: {
+                    bookingId: newBooking.id,
+                    amountMoney: 0,
+                    bankingName: "",
+                    accountNumber: "",
+                    paymentDate: null,
+                    paymentSlip: null
+                }
+            })
+
+            res.send(newBooking);
         })
-
-        //สร้าง Process
-        const newProcess = await prisma.process.create ({
-            data: {
-                bookingId: newBooking.id,
-                subServiceId: 1,
-                timestamp: new Date(),
-            }
-        })
-        
-        //สร้าง Payment
-        const newPayment = await prisma.payment.create ({
-            data: {
-                bookingId: newBooking.id,
-                amountMoney: 0,
-                bankingName: "",
-                accountNumber: "",
-                paymentDate: null,
-                paymentSlip: null
-            }
-        })
-
-        res.send("WOW")
-
-
-    })
 
     } catch (err: any) {
         res.status(err.status || 500).json({ message: err.message });
@@ -147,19 +157,19 @@ export const cancelBooking = async (req: Request, res: Response, next: NextFunct
             }
         })
 
-        const deleteSchedule = await prisma.schedules.delete ({
+        const deleteSchedule = await prisma.schedules.delete({
             where: {
                 bookingId: Number(id)
             }
         })
 
-        const deleteProcess = await prisma.process.deleteMany ({
+        const deleteProcess = await prisma.process.deleteMany({
             where: {
                 bookingId: Number(id)
             }
         })
 
-        const deletePayment = await prisma.payment.delete ({
+        const deletePayment = await prisma.payment.delete({
             where: {
                 bookingId: Number(id)
             }
@@ -172,18 +182,16 @@ export const cancelBooking = async (req: Request, res: Response, next: NextFunct
 }
 
 export const searchScheduleDateTime = async (req: Request, res: Response, next: NextFunction) => {
-    
-    const { appointmentDateTime } = req.body
-
+    const { appointmentDateTime } = req.body;
     try {
         const dateTime = await prisma.booking.findMany({
             where: {
-              appointmentDateTime: appointmentDateTime,
-            },
+                appointmentDateTime: appointmentDateTime
+            }
         });
-        res.send(dateTime)
-        
-    } catch (err:any) {
+
+        res.send(dateTime);
+    } catch (err: any) {
         res.status(err.status || 500).json({ message: err.message });
     }
 }
